1bc806245004ca53f24eb845850d9791809464b6
dan kim <donghee214@gmail.com>
2020-10-05 21:27:22 -0400

    Updates

--- src/Details/Shared/Components/UoftHacksLogo.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import UoftHacksLogo from "Shared/Resources/Images/uoftHacksLogo.png";
-
-export default () => (
-    <div>
-        <img src={UoftHacksLogo} className="logoImageContainer" />
-    </div>
-);
